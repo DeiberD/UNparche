c39ebcd420f412f1066e172a852ccbf240fb1532
Daniel Lopez <mantabu07@gmail.com>
2026-06-30 23:05:52 -0500

    Add initial D1 migrations

---
+++ database/d1/migrations/0002_seed_catalogs.sql
@@ -0,0 +1,6 @@
+INSERT INTO tipo_evento (id_tipo_evento, nombre, icono_svg) VALUES
+    (1, 'ACADEMICO', 'book.svg'),
+    (2, 'CULTURAL', 'palette.svg'),
+    (3, 'DEPORTIVO', 'ball.svg'),
+    (4, 'SOCIAL', 'users.svg'),
+    (5, 'OTRO', 'pin.svg');
